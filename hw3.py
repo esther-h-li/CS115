75d35248e8e40c5420f62db488be083dd0ad1467
esther-h-li <estherhuanneli@gmail.com>
2025-10-24 09:58:30 -0400

    Add files via upload
    Enhanced memoization

--- hw3.py
+++ hw3.py
@@ -12,36 +12,40 @@
 def empty(m, n):
     """Creates a grid with m rows and n columns"""
     return copy([[0] * n] * m)
+
 def copy(grid):
     """Returns a deep copy of the grid"""
     return list(map(lambda x: x[:], grid))
+
 def increase_row(grid, y, cost):
     """Increases every element in row y by cost"""
     grid[y] = (list(map(lambda x: x + cost, grid[y])))
+    
 def increase_col(grid, x, cost):
     """Increases every element in column x by cost"""
     def add(y):
         y[x] = y[x] + cost
     grid = list(map(add, grid))
 
-memo = {}
+
+    
 def distance_from(grid, x, y):
     """Calculates the shortest distance from a point (x,y) to (0,0)"""
-   
- 
-    
-    if(x == 0 and y == 0):
-        return grid[x][y]
-    
-    if(not x,y in memo):
-            
-        if(x==0):
-            memo[x,y]= grid[x][y] + distance_from(grid, x, y-1)
-            
-        elif(y==0):
-            memo[x,y]= grid[x][y] + distance_from(grid, x-1, y)
+    memo = {}
+    def inner_distance_from(x,y):
+        if(x == 0 and y == 0):
+            return grid[x][y]
         
-        else: memo[x,y] = grid[x][y] + min(distance_from(grid, x, y-1),  distance_from(grid, x-1, y))
-    return memo[x,y]
+        if(not (x,y) in memo):
+                
+            if(x==0):
+                memo[(x,y)]= grid[x][y] + inner_distance_from(x, y-1)
+                
+            elif(y==0):
+                memo[(x,y)]= grid[x][y] + inner_distance_from(x-1, y)
+            
+            else: memo[(x,y)] = grid[x][y] + min(inner_distance_from(x, y-1),  inner_distance_from(x-1, y))
+        return memo[(x,y)]
+    return inner_distance_from(x,y)
      
     
